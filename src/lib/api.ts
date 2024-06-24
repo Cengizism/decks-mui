63b2944ef9ffdbeaafd26aeb60a02420889f09c1
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-06-24 17:49:02 +0200

    Introduced a cache

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,20 +6,36 @@ import { CardType, CardSlugType, DeckType } from "@/interfaces/types";
 
 const contentDirectory = join(process.cwd(), "content");
 
+const cache = {
+  slugs: [] as CardSlugType[],
+  cards: new Map<string, CardType>(),
+  decks: decks,
+};
+
+function readDirectory(folderPath: string): string[] {
+  return fs.readdirSync(folderPath);
+}
+
 export function getCardSlugs(): CardSlugType[] {
-  return decks.flatMap(({ folderName }) => {
+  if (cache.slugs.length > 0) {
+    return cache.slugs;
+  }
+
+  cache.slugs = decks.flatMap(({ folderName }) => {
     const deckPath = join(contentDirectory, folderName);
-    return fs.readdirSync(deckPath).map(slug => ({
-      slug,
-      deck: folderName
+    return readDirectory(deckPath).map(slug => ({
+      slug: slug.replace(/\.mdx$/, ""),
+      deck: folderName,
     }));
   });
+
+  return cache.slugs;
 }
 
 function findDeckFolderNameByCardSlug(slug: string): string | undefined {
   for (const deck of decks) {
     const deckPath = join(contentDirectory, deck.folderName);
-    const cardFiles = fs.readdirSync(deckPath);
+    const cardFiles = readDirectory(deckPath);
 
     if (cardFiles.includes(slug + ".mdx")) {
       return deck.folderName;
@@ -35,14 +51,18 @@ export function getDeckTitle(folderName: string): string | undefined {
 
 export function getCardBySlug(slug: string): CardType | null {
   if (!slug) {
-    console.error('Invalid slug parameter:', { slug });
+    console.error("Invalid slug parameter:", { slug });
     return null;
   }
 
   const realSlug = slug.replace(/\.mdx$/, "");
+  if (cache.cards.has(realSlug)) {
+    return cache.cards.get(realSlug) || null;
+  }
+
   const deck = findDeckFolderNameByCardSlug(realSlug);
   if (!deck) {
-    console.error('Deck not found for slug:', { slug });
+    console.error("Deck not found for slug:", { slug });
     return null;
   }
   const fullPath = join(contentDirectory, deck, `${realSlug}.mdx`);
@@ -50,7 +70,9 @@ export function getCardBySlug(slug: string): CardType | null {
   try {
     const fileContents = fs.readFileSync(fullPath, "utf8");
     const { data, content } = matter(fileContents);
-    return { ...data, slug: realSlug, content, deck } as CardType;
+    const card = { ...data, slug: realSlug, content, deck } as CardType;
+    cache.cards.set(realSlug, card);
+    return card;
   } catch (error) {
     console.error(`Error reading file at path ${fullPath}:`, error);
     return null;
@@ -58,21 +80,24 @@ export function getCardBySlug(slug: string): CardType | null {
 }
 
 export function getAllCards(): CardType[] {
-  return getCardSlugs().map(({ slug }) => getCardBySlug(slug)).filter(card => card !== null) as CardType[];
+  if (cache.cards.size > 0) {
+    return Array.from(cache.cards.values());
+  }
+
+  const cards = getCardSlugs().map(({ slug }) => getCardBySlug(slug)).filter(card => card !== null) as CardType[];
+  cards.forEach(card => card && cache.cards.set(card.slug, card));
+  return cards;
 }
 
 function readCardFiles(folderName: string): string[] {
   const deckPath = join(contentDirectory, folderName);
-  const cardFiles = fs.readdirSync(deckPath);
-
-  // Optionally filter or transform cardFiles here
-  return cardFiles;
+  return readDirectory(deckPath);
 }
 
 export function getAllDecks(): DeckType[] {
   return decks.map(deck => ({
     ...deck,
-    cards: readCardFiles(deck.folderName)
+    cards: readCardFiles(deck.folderName),
   }));
 }
 
